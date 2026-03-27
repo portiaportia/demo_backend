f0f1431ebaa44620ed9a7b7b4acff8965a472ab8
portiaportia <portiaplante@gmail.com>
2026-03-27 09:50:48 -0400

    added original array of data

--- app.js
+++ app.js
@@ -1,9 +1,84 @@
-var http = require("http");
-const port = process.env.PORT || 3001;
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+      cb(null, "./public/images/");
+    },
+    filename: (req, file, cb) => {
+      cb(null, file.originalname);
+    },
+  });
+  
+  const upload = multer({ storage: storage });
 
-http
-  .createServer(function (req, res) {
-    res.writeHead(200, { "Content-Type": "text/html" });
-    res.end("Hello Hello!");
-  })
-  .listen(port);    
+let houses = [
+    {
+        "_id":1,
+        "name": "Farmhouse",
+        "size": 2000,
+        "bedrooms": 3,
+        "bathrooms": 2.5,
+        "features": [
+            "wrap around porch",
+            "attached garage"
+        ],
+        "main_image": "farm.webp",
+        "floor_plans": [
+            {
+                "name": "Main Level",
+                "image": "farm-floor1.webp"
+            },
+            {
+                "name": "Basement",
+                "image": "farm-floor2.webp"
+            }
+        ]
+    },
+    {
+        "_id":2,
+        "name": "Mountain House",
+        "size": 1700,
+        "bedrooms": 3,
+        "bathrooms": 2,
+        "features": [
+            "grand porch",
+            "covered deck"
+        ],
+        "main_image": "mountain-house.webp",
+        "floor_plans": [
+            {
+                "name": "Main Level",
+                "image": "mountain-house1.webp"
+            },
+            {
+                "name": "Optional Lower Level",
+                "image": "mountain-house2.webp"
+            },
+            {
+                "name": "Main Level Slab Option",
+                "image": "mountain-house3.jpg"
+            }
+        ]
+    },
+    {
+        "_id":3,
+        "name": "Lake House",
+        "size": 3000,
+        "bedrooms": 4,
+        "bathrooms": 3,
+        "features": [
+            "covered deck",
+            "outdoor kitchen",
+            "pool house"
+        ],
+        "main_image": "farm.webp",
+        "floor_plans": [
+            {
+                "name": "Main Level",
+                "image": "lake-house1.webp"
+            },
+            {
+                "name": "Lower Level",
+                "image": "lake-house2.webp"
+            }
+        ]
+    }
+]
